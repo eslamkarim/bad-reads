404bd3a24ac2803182d90012b7cba918041c8d88
Mohamed Atef <nerophone2012@gmail.com>
2020-05-05 07:01:34 +0200

    author v4

--- badreads-frontend/src/components/data-table.js
+++ badreads-frontend/src/components/data-table.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import './author.css'
 
 class DataTable extends Component {
+
     render() {
         return (
-            <tr>
-                <td>
-                    {this.props.obj.authorName}
-                </td>
-                <td>
-                    {this.props.obj.date_of_birth}
-                </td>
-                <td>
-                    {this.props.obj.img}
-                </td>
-            </tr>
+            <center>
+                <div className="card">
+                    <img className="img" src={this.props.obj.img} width="50px" height="50px" />
+                    <div className="container">
+                        <span>Author Name :</span> {this.props.obj.authorName}
+                        <br/>
+                        <span>Birth Date :</span>{this.props.obj.date_of_birth}
+                    </div>
+                </div>
+            </center>
         );
     }
 }
