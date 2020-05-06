ef8ff3cb711a2877301edf31508454a74a37a0b8
Eslam karim gaber <eslamkarim222@gmail.com>
2020-05-06 18:16:34 +0200

    adding GIFS :alien: :alien: :alien:

--- badreads-backend/routes/registerRouter.js
+++ badreads-backend/routes/registerRouter.js
@@ -14,7 +14,7 @@ var storage = multer.diskStorage({
 })
 var upload = multer({ storage: storage ,
     fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg"|| file.mimetype == "image/gif") {
             cb(null, true);
         } else {
             cb(null, false);
