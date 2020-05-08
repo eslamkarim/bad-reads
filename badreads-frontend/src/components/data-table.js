3982199cfb5313dd1c8c2db8235194708851a690
Mohamed Atef <nerophone2012@gmail.com>
2020-05-08 19:48:50 +0200

    commit gd3na mny kda

--- badreads-frontend/src/components/data-table.js
+++ badreads-frontend/src/components/data-table.js
@@ -18,7 +18,6 @@ class DataTable extends Component {
                     <a href={bUrl}>
                         <img className="img" src={this.props.obj.img} />
                         <h1 className="item-card-title mt-3 mb-3"> {this.props.obj.authorName}</h1>
-                        {/* <p className="card-text"><span>Birth Date :</span>{this.props.obj.date_of_birth}</p>  */}
                     </a>
                 </div>
             </div>
