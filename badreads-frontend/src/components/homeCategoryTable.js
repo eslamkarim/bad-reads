682b6200d3db7b536aececcd3818413d4444d0b8
Mohamed Atef <nerophone2012@gmail.com>
2020-05-09 05:03:37 +0200

    7aga sare3a f el home elli kareem 2ali 3aleha

--- badreads-frontend/src/components/homeCategoryTable.js
+++ badreads-frontend/src/components/homeCategoryTable.js
@@ -7,14 +7,20 @@ class HomeCategoryTable extends Component {
     render() {
         let bUrl = `http://localhost:3000/category/${this.props.obj._id}`
         return (
-            <div className="col-md-3 col-sm-6 item">
-                    <div className="card item-card card-block">
-                        <Link to={bUrl}>
-                            {/* <img className="img" src={this.props.obj.img} /> */}
-                            <h1 className="item-card-title mt-3 mb-3"> {this.props.obj.categoryName}</h1>
-                            <p className="img"><span>Birth Date :</span>{this.props.obj.categoryDescription}</p> 
-                         </Link>
-                    </div>
+            <div className="">
+                <div className="card item-card-cat card-block">
+                    <Link to={bUrl}>
+                        <br />
+                        <h1 className="name-item">Category Name </h1>
+                        <h1 className="item-card-bookname">{this.props.obj.categoryName} </h1>
+                        <br />
+                        <h1 className="name-item">Description</h1>
+                        <h1 className="item-card-bookdis"> {this.props.obj.categoryDescription}</h1>
+                        <br />
+                        <img className="img-cattegory" src={require('../category.png')} />
+
+                    </Link>
+                </div>
             </div>
         );
     }
